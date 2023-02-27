cd4ae6e4ec649ace22f49b86d3d144d1de2e1a84
Neil Sullivan <ac30delay@gmail.com>
2023-02-27 10:44:24 -0600

    Phase 3 complete

--- Program.cs
+++ Program.cs
@@ -6,34 +6,24 @@ namespace Guessing_Game
     {
         static void Main(string[] args)
         {
-            // Display a message to the user asking them to guess the secret number.
-            //Display a prompt for the user's guess.
+
             Console.WriteLine("Guess a number");
-            //Take the user's guess as input and save it as a variable.
-            string input = Console.ReadLine();
-            int parsedInput = int.Parse(input);
-
-            // Display the user's guess back to the screen.
-            // No longer display the user's guess. They know what they guessed, right?
-            //Console.WriteLine(input);
-
-            //Create a variable to contain the secret number(application's number). 
-            //This number should be hard-coded for now. 42 is a nice number.
             int secretNumber = 42;
-
-            //Compare the user's guess with the secret number. 
-            //Display a success message if the guess is correct, otherwise display a failure message.
-            if (parsedInput == secretNumber)
+            // Give the user four chances to guess the number.
+            for (int i = 0; i < 4; i++)
             {
-                Console.WriteLine("You Guessed Right!");
+                string input = Console.ReadLine();
+                int parsedInput = int.Parse(input);
+                //Continue to display the success or failure messages as in phase 2
+                if (parsedInput == secretNumber)
+                {
+                    Console.WriteLine("You Guessed Right!");
+                }
+                else
+                {
+                    Console.WriteLine("You Guessed Wrong :(");
+                }
             }
-            else
-            {
-                Console.WriteLine("You Guessed Wrong :(");
-            }
-
-
-
         }
     }
 }
