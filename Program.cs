b224f503b62f0044c78156d605b1009be31fc9c2
Neil Sullivan <ac30delay@gmail.com>
2023-02-27 13:04:33 -0600

    Phase 5 complete

--- Program.cs
+++ Program.cs
@@ -2,27 +2,28 @@
 
 
 
-//Phase 4:
-// Display the number of the user's current guess in the prompt. For example, if the user has already guessed one time, the prommpt should say something like Your guess (2)>.
-//End the loop early if the user guesses the correct number.
+//Phase 5:
+// Use a random number to set the secret number between 1 and 100 instead of a hard-coded number.
+//The prompt should display the number of guesses the user has left.
 
 
 Console.WriteLine("Guess a number, you have 4 chances.");
-int secretNumber = 42;
-// Give the user four chances to guess the number.
+int secretNumber = new Random().Next(1, 100);
+
 for (int i = 0; i < 4; i++)
 {
+    int guesses = 4 - (i + 1);
     string input = Console.ReadLine();
     int parsedInput = int.Parse(input);
     //Continue to display the success or failure messages as in phase 2
     if (parsedInput == secretNumber)
     {
-        Console.WriteLine($"You Guessed Right! Guess Number: {i + 1}");
+        Console.WriteLine($"You Guessed Right! Guesses remaining: {guesses}");
         break;
     }
     else
     {
-        Console.WriteLine($"You Guessed Wrong :(, Guess Number: {i + 1}");
+        Console.WriteLine($"You Guessed Wrong :(, Guesses remaining: {guesses}");
     }
 }
 
