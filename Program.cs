f17ac76281d0b57a38f30c7665a485e63f348c74
Neil Sullivan <ac30delay@gmail.com>
2023-02-27 15:59:09 -0600

    Phases 6,7,8 complete

--- Program.cs
+++ Program.cs
@@ -2,17 +2,39 @@
 
 
 
-//Phase 5:
-// Use a random number to set the secret number between 1 and 100 instead of a hard-coded number.
-//The prompt should display the number of guesses the user has left.
+//Phase 6:
+// Inform the user if their guess was too high or too low, when they guess incorrectly.
 
+int y = 0;
 
-Console.WriteLine("Guess a number, you have 4 chances.");
-int secretNumber = new Random().Next(1, 100);
-
-for (int i = 0; i < 4; i++)
+Console.WriteLine("Guess a number, do you want easy, hard, or medium difficulty.");
+Console.WriteLine("You can also have `cheater` mode which give you infinite guesses");
+string difficulty = Console.ReadLine();
+if (difficulty == "easy")
 {
-    int guesses = 4 - (i + 1);
+    y = 8;
+};
+if (difficulty == "hard")
+{
+    y = 4;
+};
+if (difficulty == "medium")
+{
+    y = 6;
+};
+if (difficulty == "cheater")
+{
+    y = 999;
+}
+
+int secretNumber = new Random().Next(1, 100);
+Console.WriteLine(secretNumber);
+
+Console.WriteLine("Guess Your Number!");
+
+for (int i = 0; i < y; i++)
+{
+    int guesses = y - (i + 1);
     string input = Console.ReadLine();
     int parsedInput = int.Parse(input);
     //Continue to display the success or failure messages as in phase 2
@@ -21,11 +43,13 @@ for (int i = 0; i < 4; i++)
         Console.WriteLine($"You Guessed Right! Guesses remaining: {guesses}");
         break;
     }
+    if (parsedInput < secretNumber)
+    {
+        Console.WriteLine($"Too low try again,  Guesses remaining: {guesses}");
+    }
     else
     {
-        Console.WriteLine($"You Guessed Wrong :(, Guesses remaining: {guesses}");
+        Console.WriteLine($"Too high try again,  Guesses remaining: {guesses}");
     }
 }
-
-
 
